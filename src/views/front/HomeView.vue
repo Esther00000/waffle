c31973a017c271b4bc83b4a1f83bc99964193733
Esther00000 <102310640+Esther00000@users.noreply.github.com>
2023-06-26 23:12:59 +0800

    Update HomeView.vue

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -2,7 +2,7 @@
     <header
         class="header d-flex justify-content-center align-items-lg-center px-4 py-5 vh-100"
         style="
-          background-image: url("https://images.unsplash.com/photo-1562513872-634b8fae6dbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80");
+          background-image: url(../../assets/images/selectedProduct/selectedProduct03.png);
           background-position: center;
           background-size: cover;
           background-color: rgba(0, 0, 0, 0.6);
